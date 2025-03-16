a1e12065807cb3542f22bb84fa4f4ab7254b3c40
Dmytro0804 <6273216@stud.kai.edu.ua>
2025-03-16 09:09:39 +0200

    PHP Налаштування профілю перегляду і зміни інформації про акаунт

--- setting.php
+++ setting.php
@@ -81,7 +81,6 @@ return true;
         <div class="content">
          <?php include_once('includes/header.php');?>
 
-
             <!-- Form Start -->
             <div class="container-fluid pt-4 px-4">
                 <div class="row g-4">
